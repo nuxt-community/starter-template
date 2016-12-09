25c73165410bd5252e0eb9e8d3b24a39ba8f6be9
Alexandre Chopin <wikeo65@gmail.com>
2016-12-09 15:54:18 +0100

    put variables

--- pages/about.vue
+++ pages/about.vue
@@ -5,7 +5,7 @@
     -->
     <img src="../assets/img/logo.png" alt="Nuxt.js Logo" />
     <h1 class="title">
-      This page is loaded from the starter
+      This page is loaded from the {{ name }}
     </h1>
     <h2 class="info" v-if="name === 'client'">
       Please refresh the page
