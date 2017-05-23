1fb7c78f6139206655b6961bbb0cf5e9daea2375
HANATANI Takuma <mail@potato4d.me>
2017-05-23 13:58:16 +0900

    Fix spelling.
    `s/Github/GitHub/`

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -10,7 +10,7 @@
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">Github</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
