ba1e9b24d2c80a4f9b5a5fa472675d80c4793091
Stefan Buhrmester <buhrmi@gmail.com>
2017-07-31 17:24:09 -0700

    use provided project details in generated code

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -3,10 +3,10 @@
     <div>
       <logo/>
       <h1 class="title">
-        NUXT
+        {{ name }}
       </h1>
       <h2 class="subtitle">
-        Universal Vue.js Application
+        {{ description }}
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
