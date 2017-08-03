fe69ec5b8d0268db823a49bc0536d6301231d55e
Sébastien Chopin <seb@chopin.io>
2017-08-03 10:35:04 +0200

    Use eslint-loader only in dev mode

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -25,7 +25,7 @@ module.exports = {
     ** Run ESLINT on save
     */
     extend (config, ctx) {
-      if (ctx.isClient) {
+      if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
