a2d4038ef3ca1965cfce08d245b47936f305afd7
NicoPennec <nemohp@yahoo.fr>
2018-01-09 00:08:08 +0100

    Fix deprecated notation

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -24,8 +24,8 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, ctx) {
-      if (ctx.dev && ctx.isClient) {
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
